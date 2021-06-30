9a793198b047d3feab934c8fcfca0e8bc6af5db0
Pedro Henrique <pedropires2603@gmail.com>
2021-06-30 19:29:21 -0300

    Criado o arquivo de testes

---
+++ tests.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "matrix/GRAPHmatrix.c"
+// #include "list/GRAPHlist.c"
+
+int main(){
+    /**
+     * Para realizar testes, basta descomentar a implementação do grafo desejada
+     * e executar as funções. TODAS as funções funcionam tanto com a representação por
+     * matriz quanto por lista, ou seja, não é necessário mudar nenhuma função para
+     * testar os mesmos resultados para as duas.
+     * 
+     * Para compilar (testado apenas no linux):
+     *      No terminal na pasta src/ digite: gcc tests.c -o tests && ./tests
+     */
+    return 0;
+}
